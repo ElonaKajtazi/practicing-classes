884ce9a7563b5e0036b0d0cef55474bbb9a4e080
Elona Kajtazi <MY_NAME@example.com>
2022-11-15 22:50:28 +0100

    created a dog class and some dog instances

--- index.ts
+++ index.ts
@@ -5,12 +5,12 @@ class Person {
   name: string;
   age: number;
   hungry: boolean;
-  tired: boolean
+  tired: boolean;
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
-    this.hungry = false
-    this.tired = false
+    this.hungry = false;
+    this.tired = false;
   }
   greet() {
     console.log(`Hello I am ${this.name}!`);
@@ -18,48 +18,48 @@ class Person {
   salute() {
     console.log("See you later!");
   }
-  checkStatus (){
-    if(this.hungry) console.log("I am hungry")
-    else console.log("I am not hungry")
-    if(this.tired) console.log("I am tired")
-    else console.log("I am not tired")
+  checkStatus() {
+    if (this.hungry) console.log("I am hungry");
+    else console.log("I am not hungry");
+    if (this.tired) console.log("I am tired");
+    else console.log("I am not tired");
   }
-  workout(){
-    console.log("1, 2, 3 runnnn")
-    this.hungry = true
-    this.tired = true
+  workout() {
+    console.log("1, 2, 3 runnnn");
+    this.hungry = true;
+    this.tired = true;
   }
-  sleep () {
-    console.log("ZzZzZzZzzZ")
-    this.tired = false
-    this.hungry = true
+  sleep() {
+    console.log("ZzZzZzZzzZ");
+    this.tired = false;
+    this.hungry = true;
   }
-  eat () {
-    console.log("Eatinggg")
-    this.hungry = false
+  eat() {
+    console.log("Eatinggg");
+    this.hungry = false;
   }
-  growUp () {
-    this.age++
+  growUp() {
+    this.age++;
   }
 }
 class Warrior extends Person {
-    // name: string;
-    // age: number;
-    weapon:String
-    constructor(name: string, age: number, weapon: string) {
-        super(name, age)
-        this.weapon = weapon
-    }
-    fight() {
-        console.log(`Taste my ${this.weapon}!`)
-    }
+  // name: string;
+  // age: number;
+  weapon: String;
+  constructor(name: string, age: number, weapon: string) {
+    super(name, age);
+    this.weapon = weapon;
+  }
+  fight() {
+    console.log(`Taste my ${this.weapon}!`);
+  }
 }
 //These are INSTANCES
 let person = new Person("Nicolas", 34);
-let warrior = new Warrior("Elona", 26, "Sord")
+let warrior = new Warrior("Elona", 26, "Sord");
 // let person2 = new Person("Elona", 26)
-warrior.fight()
-warrior.greet()
+warrior.fight();
+warrior.greet();
 // person.greet();
 // person.checkStatus()
 // person.workout()
@@ -68,3 +68,27 @@ warrior.greet()
 // person.checkStatus()
 // person.eat()
 // person.checkStatus()
+
+class Dog {
+  breed: string;
+  name: string;
+  age: number;
+
+  constructor(breed: string, name: string) {
+    this.breed = breed;
+    this.name = name;
+    this.age = 0;
+  }
+  bark() {
+    console.log("Woof woof");
+  }
+  growup() {
+    this.age++;
+  }
+}
+const dog = new Dog("Chihuahua", "Fluffy");
+const dog1 = new Dog("Golden Retriever", "Barky");
+
+dog.growup()
+console.log(dog)
+console.log(dog1)
